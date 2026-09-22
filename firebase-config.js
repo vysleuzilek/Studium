44668990ef9a2d15eb07a85443fa8bf608dd7bf4
vysleuzilek <vyslouzil.karlovka@gmail.com>
2026-09-22 18:15:36 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TVOJE_API_KEY",
-  authDomain: "TVUJ_PROJEKT.firebaseapp.com",
-  projectId: "TVUJ_PROJEKT",
-  storageBucket: "TVUJ_PROJEKT.appspot.com",
-  messagingSenderId: "TVOJE_ID",
-  appId: "TVOJE_APP_ID"
+  apiKey: "AIzaSyAZYgn-_DRK6sCHDS2C5UH5TzYB0qNppDQ",
+  authDomain: "studijni-prehled.firebaseapp.com",
+  databaseURL: "https://studijni-prehled-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "studijni-prehled",
+  storageBucket: "studijni-prehled.firebasestorage.app",
+  messagingSenderId: "692217559448",
+  appId: "1:692217559448:web:6a522624e7e72a57a413ed"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
